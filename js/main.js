0efa02b61f615b9204872b5079275dd710cf97e5
kacperlechicki <klechicki@poczta.fm>
2023-01-03 20:46:06 +0100

    first drawing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-"use strict";
+'use strict';
+
+const canvas = document.querySelector('canvas');
+
+let isDrawing = false;
+
+//this method returns a drawing context on the canvas
+const ctx = canvas.getContext('2d');
+
+window.addEventListener('load', () => {
+	//draw at exact position of mouse
+	canvas.width = canvas.offsetWidth;
+	canvas.height = canvas.offsetHeight;
+});
+
+const drawing = (e) => {
+	if (!isDrawing) return; //if isDrawing is false return from here
+
+	//this method create a new line
+	ctx.lineTo(e.offsetX, e.offsetY);
+
+	//drawing and filling line with color
+	ctx.stroke();
+};
+
+canvas.addEventListener('mousemove', drawing);
